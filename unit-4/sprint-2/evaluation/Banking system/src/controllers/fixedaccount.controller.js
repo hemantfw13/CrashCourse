9d5a0bcaa677cf461b606cbf637308b3151c5e10
hemantahire002 <93426308+hemantahire002@users.noreply.github.com>
2022-02-07 17:18:27 +0530

    new4

--- unit-4/sprint-2/evaluation/Banking system/src/controllers/fixedaccount.controller.js
+++ unit-4/sprint-2/evaluation/Banking system/src/controllers/fixedaccount.controller.js
@@ -13,4 +13,13 @@ router.get("/fixed", async (req, res) => {
   const fixed = await fixedaccountSchema.find().lean().exec();
   res.send(fixed);
 });
+
+router.patch("/fixed", async (req, res) => {
+  const fixed = await fixedaccountSchema.findOneAndUpdate(
+    {},
+    { $set: { balance: 0000 } }
+  );
+  res.send(fixed);
+});
+
 module.exports = router;
